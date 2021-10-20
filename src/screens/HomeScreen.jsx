b94b09bda14cdb4ce56f12ccb906a0365fd02a7d
Amil Abdullayev <amila@goldenpay.az>
2021-10-20 17:56:16 +0400

    some changes were added

---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from "react";
+
+
+class HomeScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date()
+        };
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default HomeScreen;
